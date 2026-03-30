e12b1111f772bc97fb2fbb1f73e5aa0492138099
LucasAraujo1016 <lucas.allbuquerque1234@gmail.com>
2026-03-30 12:00:35 -0300

    feat: BB Criação front-end da tela de login

--- Bento-Box/app/login.tsx
+++ Bento-Box/app/login.tsx
@@ -1,15 +1,26 @@
 import { Component } from "react";
-import {Text, View } from "react-native";
+import {Image, Pressable, Text, TextInput, View } from "react-native";
 import style from "./styleSheet";
 
 export default class Login extends Component {
     render (){
         return (
-            <View style={{flexDirection: 'column'}}>
-                <View style={style.header}>
-                    <Text style={{
-                    }}>
-                    Bento-Box</Text>
+            <View style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '50'}}>
+                <View style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+                    <Image source={require('../assets/images/logo.png')} style={{width: 250, height: 250, justifyContent: "center", alignItems: 'center'}} />
+                    <Text style={{fontSize: 50, fontWeight: 'bold'}}>Bento-Box</Text>
+                </View>
+                <View style={{flexDirection: 'column', gap: '20'}}>
+                    <TextInput style={style.input_login} placeholder="Nome de Usuário/E-mail"></TextInput>
+                    <TextInput style={style.input_login} placeholder="Senha"></TextInput>
+                </View>
+                <View style={{flexDirection: 'column', gap: '20'}}>
+                    <Pressable style={style.login_button}>
+                        <Text style={{fontSize: 25}}>Entrar</Text>
+                    </Pressable>
+                    <Pressable style={style.login_button}>
+                        <Text style={{fontSize: 25}}>Cadastrar</Text>
+                    </Pressable>
                 </View>
             </View>
         )
--- Bento-Box/app/styleSheet.tsx
+++ Bento-Box/app/styleSheet.tsx
@@ -8,6 +8,25 @@ const style = StyleSheet.create ({
         height: 117,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    input_login: {
+        backgroundColor: '#D9D9D9',
+        width: 302,
+        height: 58,
+        borderRadius: 10,
+        fontWeight: 'bold',
+        paddingLeft: 10
+    },
+
+    login_button: {
+        backgroundColor: '#FF9D4D',
+        width: 302,
+        height: 58,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        fontWeight: 'bold',
     }
 })
 
